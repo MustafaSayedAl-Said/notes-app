e69a6beeb17af405c7610c71c4afaa3914535894
MustafaSayedAl-Said <mostafa_alsaid2011@hotmail.com>
2023-08-06 08:54:12 +0300

    Create custom text field

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -9,12 +9,18 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body:  const NotesViewBody(),
+        body: const NotesViewBody(),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            showModalBottomSheet(context: context, builder: (context){
-              return const AddNoteBottomSheet();
-            });
+            showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              },
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+            );
           },
           child: const Icon(Icons.add),
         ),
--- lib/views/show_bottom_modal_sheet.dart
+++ lib/views/show_bottom_modal_sheet.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
-
+import 'custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 32.0,
+          ),
+          CustomTextField(),
+        ],
+      ),
+    );
   }
 }
